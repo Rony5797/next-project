17c6821ccd097f1c9e6ade36feceb029b13b6a64
Rony Hossain <rooneyhossain@gmail.com>
2024-05-09 22:57:39 +0600

    query update

--- db/queries.js
+++ db/queries.js
@@ -3,21 +3,26 @@ import UserModel from "@/models/userModel";
 import mongoose from "mongoose";
 
 async function getAllrecipe() {
+  await RecipeModel();
   const allRecipe = await RecipeModel.find();
   return allRecipe;
 }
 async function getRecipeById(id) {
+  await RecipeModel();
   const Recipe = await RecipeModel.findById(id);
   return Recipe;
 }
 async function createUser(user) {
+  await UserModel();
   return await UserModel.create(user);
 }
 async function findUserByCredential(credentials) {
+  await UserModel();
   const user = await UserModel.findOne(credentials).lean();
   return user;
 }
 async function recipeFav(userId, recepeId) {
+  await UserModel();
   const userFav = await UserModel.findById(userId);
   if (userFav) {
     const foundRecipe = userFav.favourites.find(
